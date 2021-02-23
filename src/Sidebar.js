1d0ad40a57107afc5bc8cf71a15fa418cfbfdd8e
Amy <aljustice25@gmail.com>
2021-02-24 12:32:03 +1300

    Update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,24 +6,24 @@ import { faHome, faWallet, faCoins } from '@fortawesome/free-solid-svg-icons'
 function Sidebar( {items} ) {
   return (
     <div class="flex flex-col sm:flex-row sm:justify-around">
-        <div class="sm:w-40 lg:w-64 h-full bg-white">
+        <div class="sm:w-1/5 min-w-min lg:w-64 h-full bg-white">
             <nav class="mt-10">
                 <a class="flex items-center py-2 px-8 bg-gray-200 text-gray-700 border-r-4 border-gray-700" href="#">
                     <FontAwesomeIcon icon={faHome} />
 
-                    <span class="mx-4 font-medium md:text-sm">Home</span>
+                    <span class="mx-4 font-medium text-sm lg:text-base">Home</span>
                 </a>
 
                 <a class="flex items-center mt-5 py-2 px-8 text-gray-600 border-r-4 border-white hover:bg-gray-200 hover:text-gray-700 hover:border-gray-700" href="#">
                     <FontAwesomeIcon icon={faWallet} />
 
-                    <span class="mx-4 font-medium md:text-sm">Pockets</span>
+                    <span class="mx-4 font-medium text-sm lg:text-base">Pockets</span>
                 </a>
 
                 <a class="flex items-center mt-5 py-2 px-8 text-gray-600 border-r-4 border-white hover:bg-gray-200 hover:text-gray-700 hover:border-gray-700" href="#">
                     <FontAwesomeIcon icon={faCoins} />
 
-                    <span class="mx-4 font-medium md:text-sm">Budget</span>
+                    <span class="mx-4 font-medium text-sm lg:text-base">Budget</span>
                 </a>
             </nav>
 
