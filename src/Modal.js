ce676afe938d10c7db8399325b20c246434b8998
Amy <aljustice25@gmail.com>
2021-02-25 15:55:47 +1300

    Add modals to pockets

---
+++ src/Modal.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Modal extends React.Component {
+
+    onClose = e => {
+        this.props.onClose && this.props.onClose(e);
+    };
+
+    render() {
+        if (!this.props.show) {
+            return null;
+          }
+          return (
+            <div class="h-screen w-screen bg-opacity-70 bg-gray-700 top-0 left-0 absolute justify-center flex items-center">
+                <div class="bg-white rounded shadow p-8 m-4 max-w-md max-h-full text-center" id="modal">
+                    <h2 class="text-xl mb-4">{this.props.label} Breakdown</h2>
+                    <div class="border-b-2 border-gray-200 my-2 py-2 font-normal w-80">
+                        <div class="inline-block w-60 text-left">Budget</div>
+                        <div class="inline w-20 text-right">$0</div>
+                    </div>
+                    <div class="border-b-2 border-gray-200 my-2 py-2 font-normal w-80">
+                        <div class="inline-block w-60 text-left">Budget</div>
+                        <div class="inline w-20 text-right">$0</div>
+                    </div>
+                    <div class="actions">
+                        <button class="toggle-button" onClick={this.onClose}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+Modal.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    show: PropTypes.bool.isRequired
+  };
--- src/PocketPanel.js
+++ src/PocketPanel.js
@@ -1,16 +1,30 @@
 import React from 'react'
+import Modal from './Modal'
 
-function PocketPanel({ label }) {
-  return (
-    <div class="bg-white rounded-lg p-4 border-2 h-24 font-semibold text-gray-700 text-center align-middle">
-        <span class="text-2xl inline-block align-text-middle">
-            00 / 00
-        </span>
-        <div class="align-text-bottom text-gray-400 mt-2">
-            { label }
-        </div>
-    </div>
-  )
+export default class PocketPanel extends React.Component {
+    state = {
+        show: false
+      };
+      showModal = e => {
+        this.setState({
+          show: !this.state.show
+        });
+      };
+
+    render() {
+        return (
+            <div class="bg-white rounded-lg p-4 border-2 h-24 font-semibold text-gray-700 text-center align-middle cursor-pointer" onClick={e => {
+                this.showModal(e);
+              }} >
+                <span class="text-2xl inline-block align-text-middle">
+                    00 / 00
+                </span>
+                <div class="align-text-bottom text-gray-400 mt-2">
+                    { this.props.label }
+                </div>
+                 <Modal label={this.props.label} onClose={this.showModal} show={this.state.show} />
+            </div>
+        )
+    }
+
 }
-
-export default PocketPanel
