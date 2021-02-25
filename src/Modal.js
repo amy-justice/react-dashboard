9b9760eeee13d2e87b5794bef660b438ac8bd539
Amy <aljustice25@gmail.com>
2021-02-25 16:02:49 +1300

    Adding some colour

--- src/Modal.js
+++ src/Modal.js
@@ -15,16 +15,16 @@ export default class Modal extends React.Component {
             <div class="h-screen w-screen bg-opacity-70 bg-gray-700 top-0 left-0 absolute justify-center flex items-center">
                 <div class="bg-white rounded shadow p-8 m-4 max-w-md max-h-full text-center" id="modal">
                     <h2 class="text-xl mb-4">{this.props.label} Breakdown</h2>
-                    <div class="border-b-2 border-gray-200 my-2 py-2 font-normal w-80">
+                    <div class="border-b-2 border-gray-100 my-2 py-2 font-normal w-80">
                         <div class="inline-block w-60 text-left">Budget</div>
                         <div class="inline w-20 text-right">$0</div>
                     </div>
-                    <div class="border-b-2 border-gray-200 my-2 py-2 font-normal w-80">
+                    <div class="border-b-2 border-gray-100 my-2 py-2 font-normal w-80">
                         <div class="inline-block w-60 text-left">Budget</div>
                         <div class="inline w-20 text-right">$0</div>
                     </div>
                     <div class="actions">
-                        <button class="toggle-button" onClick={this.onClose}>
+                        <button class="bg-purple-500 hover:bg-purple-700 text-white font-bold mt-6 py-2 px-4 rounded" onClick={this.onClose}>
                             Close
                         </button>
                     </div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,20 +8,20 @@ function Sidebar( {items} ) {
     <div class="flex flex-col sm:flex-row sm:justify-around">
         <div class="sm:w-1/5 min-w-min lg:w-64 h-full bg-white">
             <nav class="mt-10">
-                <a class="flex items-center py-2 px-8 bg-gray-200 text-gray-700 border-r-4 border-gray-700" href="#">
-                    <FontAwesomeIcon icon={faHome} />
+                <a class="flex items-center py-2 px-8 bg-gray-200 text-gray-700 border-r-4 border-purple-600" href="#">
+                    <div class="text-purple-800"><FontAwesomeIcon icon={faHome} /></div>
 
                     <span class="mx-4 font-medium text-sm hidden md:inline lg:text-base">Home</span>
                 </a>
 
-                <a class="flex items-center mt-5 py-2 px-8 text-gray-600 border-r-4 border-white hover:bg-gray-200 hover:text-gray-700 hover:border-gray-700" href="#">
-                    <FontAwesomeIcon icon={faWallet} />
+                <a class="flex items-center mt-5 py-2 px-8 text-gray-600 border-r-4 border-white hover:bg-gray-200 hover:text-gray-700 hover:border-purple-600" href="#">
+                    <div class="text-purple-800"><FontAwesomeIcon icon={faWallet} /></div>
 
                     <span class="mx-4 font-medium text-sm hidden md:inline lg:text-base">Pockets</span>
                 </a>
 
-                <a class="flex items-center mt-5 py-2 px-8 text-gray-600 border-r-4 border-white hover:bg-gray-200 hover:text-gray-700 hover:border-gray-700" href="#">
-                    <FontAwesomeIcon icon={faCoins} />
+                <a class="flex items-center mt-5 py-2 px-8 text-gray-600 border-r-4 border-white hover:bg-gray-200 hover:text-gray-700 hover:border-purple-600" href="#">
+                    <div class="text-purple-800"><FontAwesomeIcon icon={faCoins} /></div>
 
                     <span class="mx-4 font-medium text-sm hidden md:inline lg:text-base">Budget</span>
                 </a>
